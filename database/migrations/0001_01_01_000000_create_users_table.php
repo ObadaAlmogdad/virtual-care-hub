48943d39cf9a59ad581bbab88067f7dcdeff67df
Anas_Alfattal <anas.ft.tcc@gmail.com>
2025-08-31 03:10:23 +0300

    update php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phoneNumber');
-            $table->string('photoPath');
+            $table->string('photoPath')->nullable();
             $table->string('address');
             $table->date('birthday');
             $table->string('gender');
